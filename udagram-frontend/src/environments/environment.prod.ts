a677c7287683bf36ff911ec1d1f2bbd756f43e7a
Daniel Nguyen <tuongdang0406@gmail.com>
2024-09-10 21:06:42 +0700

    Update backend cluster url

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a6248ae0527474ac4908d9a0780a1fa3-572157705.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
